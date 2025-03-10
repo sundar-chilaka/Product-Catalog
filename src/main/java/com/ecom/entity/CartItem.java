42fcc1cbba26788046aa95cb26a9c3c7f2ff036a
sundar-chilaka <sundar3.ch@gmail.com>
2025-03-10 19:34:07 +0530

    Adding cratItem Entity,controller,Service,Repository code

---
+++ src/main/java/com/ecom/entity/CartItem.java
@@ -0,0 +1,27 @@
+package com.ecom.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "tb_cart-Item")
+public class CartItem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	private int quantity;
+
+}
